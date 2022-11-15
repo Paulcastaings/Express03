552a2a599366903b09efa5869d08ea2c54826b77
Paul Castaings <castaings.paul@gmail.com>
2022-11-15 10:19:56 +0100

    express04 done

--- userHandlers.js
+++ userHandlers.js
@@ -1,4 +1,34 @@
 const database = require("./database");
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+const getUserById = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    database
+      .query("select * from users where id = ?", [id])
+      .then(([users]) => {
+        if (users[0] != null) {
+          res.json(users[0]);
+        } else {
+          res.status(404).send("Not Found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+    };
+
 const postUser = (req, res) => {
     const { firstname, lastname, email, city, language } = req.body;
     database
@@ -14,8 +44,33 @@ const postUser = (req, res) => {
         res.status(500).send("Error saving the movie");
       });
   };
+
+  const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { firstname, lastname, email, city, language} = req.body;
+  
+    database
+      .query(
+        "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+        [firstname, lastname, email, city, language, id]
+      )
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.status(404).send("Not Found");
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error editing the user");
+      });
+  };
+  
   
   module.exports = {
     postUser,
+    updateUser,
+    getUsers,
+    getUserById,
   };
-  
